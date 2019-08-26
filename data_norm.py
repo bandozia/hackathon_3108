7b2da7fa4baada1dcceaedc699a55633f52c634a
Bruno Andozia <bruno@antidotodesign.com.br>
2019-08-26 15:03:21 -0300

    teste dbscan

--- data_norm.py
+++ data_norm.py
@@ -1,17 +1,6 @@
 import numpy as np
 import pandas as pd
 
-lojas_df = pd.read_csv("data/lojas_enc.csv")
-#lojas_df.drop(['encrypted_domain','cnae'], axis=1, inplace=True)
-lojas_df.encrypted_5_zipcode = pd.to_numeric(lojas_df.encrypted_5_zipcode, errors='coerce')
-
-lojas_df.encrypted_5_zipcode = (lojas_df.encrypted_5_zipcode - lojas_df.encrypted_5_zipcode.min()) / (lojas_df.encrypted_5_zipcode.max() - lojas_df.encrypted_5_zipcode.min())
-lojas_df.produtos_vendidos = (lojas_df.produtos_vendidos - lojas_df.produtos_vendidos.min()) / (lojas_df.produtos_vendidos.max() - lojas_df.produtos_vendidos.min())
-lojas_df.faturamento_total = (lojas_df.faturamento_total - lojas_df.faturamento_total.min()) / (lojas_df.faturamento_total.max() - lojas_df.faturamento_total.min())
-lojas_df.transacoes_total = (lojas_df.transacoes_total - lojas_df.transacoes_total.min()) / (lojas_df.transacoes_total.max() - lojas_df.transacoes_total.min())
-
-lojas_df.to_csv("data/lojas_norm.csv", index=False)
-
 print("gerando base de transacoes de pessoa fisica e juridica")
 trans_df = pd.read_csv("data/transacoes.csv")
 trans_df['dateTime'] = pd.to_datetime(trans_df['dateTime'], errors='coerce')
@@ -19,9 +8,6 @@ trans_df['periodo_venda_norm_4'] = trans_df.dateTime.apply(lambda x: round(((x.h
 trans_df['productTotal'] = pd.to_numeric(trans_df['productTotal'], errors='coerce')
 
 trans_pf = trans_df.dropna(subset=['encrypted_buyer_cpf'])
-trans_pj = trans_df.dropna(subset=['encrypted_buyer_cnpj'])
-
-((lojas_df.dinheiro + lojas_df.debito + lojas_df.deposito + lojas_df.transferencia + lojas_df.credito + lojas_df.cheque + lojas_df.crediario) == 1).value_counts()
 
 clientes_df = pd.DataFrame()
 total_produtos = trans_pf.encrypted_buyer_cpf.value_counts()
@@ -34,12 +20,12 @@ clientes_df['valor_total'] = clientes_df.cpf.apply(lambda x : valor_total.loc[x]
 per_0 = trans_pf[trans_pf.periodo_venda_norm_4 == 0].encrypted_buyer_cpf.value_counts()
 per_1 = trans_pf[trans_pf.periodo_venda_norm_4 == 1].encrypted_buyer_cpf.value_counts()
 per_2 = trans_pf[trans_pf.periodo_venda_norm_4 == 2].encrypted_buyer_cpf.value_counts()
-per_3 = trans_pf[trans_pf.periodo_venda_norm_4 == 2].encrypted_buyer_cpf.value_counts()
-per_4 = trans_pf[trans_pf.periodo_venda_norm_4 == 2].encrypted_buyer_cpf.value_counts()
+per_3 = trans_pf[trans_pf.periodo_venda_norm_4 == 3].encrypted_buyer_cpf.value_counts()
+per_4 = trans_pf[trans_pf.periodo_venda_norm_4 == 4].encrypted_buyer_cpf.value_counts()
 
 def count_sazon(cpf, periodo):
-    if cpf in per_0.index:
-        return per_0.loc[cpf]
+    if cpf in periodo.index:
+        return periodo.loc[cpf]
     else:
         return 0
 
@@ -49,4 +35,35 @@ clientes_df['periodo_2'] = clientes_df.cpf.apply(lambda x : count_sazon(x, per_2
 clientes_df['periodo_3'] = clientes_df.cpf.apply(lambda x : count_sazon(x, per_3))
 clientes_df['periodo_4'] = clientes_df.cpf.apply(lambda x : count_sazon(x, per_4))
 
-clientes_df
+clientes_df.to_csv("data/clientes_pf.csv", index=False)
+
+lojas_df = pd.read_csv("data/lojas_enc.csv")
+#lojas_df.drop(['encrypted_domain','cnae'], axis=1, inplace=True)
+lojas_df.encrypted_5_zipcode = pd.to_numeric(lojas_df.encrypted_5_zipcode, errors='coerce')
+
+lojas_df.encrypted_5_zipcode = (lojas_df.encrypted_5_zipcode - lojas_df.encrypted_5_zipcode.min()) / (lojas_df.encrypted_5_zipcode.max() - lojas_df.encrypted_5_zipcode.min())
+lojas_df.produtos_vendidos = (lojas_df.produtos_vendidos - lojas_df.produtos_vendidos.min()) / (lojas_df.produtos_vendidos.max() - lojas_df.produtos_vendidos.min())
+lojas_df.faturamento_total = (lojas_df.faturamento_total - lojas_df.faturamento_total.min()) / (lojas_df.faturamento_total.max() - lojas_df.faturamento_total.min())
+lojas_df.transacoes_total = (lojas_df.transacoes_total - lojas_df.transacoes_total.min()) / (lojas_df.transacoes_total.max() - lojas_df.transacoes_total.min())
+
+((lojas_df.dinheiro + lojas_df.debito + lojas_df.deposito + lojas_df.transferencia + lojas_df.credito + lojas_df.cheque + lojas_df.crediario) == 1).value_counts()
+
+per_loja_0 = trans_df[trans_df.periodo_venda_norm_4 == 0].encrypted_cnpj.value_counts()
+per_loja_1 = trans_df[trans_df.periodo_venda_norm_4 == 1].encrypted_cnpj.value_counts()
+per_loja_2 = trans_df[trans_df.periodo_venda_norm_4 == 2].encrypted_cnpj.value_counts()
+per_loja_3 = trans_df[trans_df.periodo_venda_norm_4 == 3].encrypted_cnpj.value_counts()
+per_loja_4 = trans_df[trans_df.periodo_venda_norm_4 == 4].encrypted_cnpj.value_counts()
+
+def count_sazon_loja(cnpj, periodo):
+    if cnpj in periodo.index:
+        return periodo.loc[cnpj]
+    else:
+        return 0
+
+lojas_df['periodo_0'] = lojas_df.encrypted_cnpj.apply(lambda x : count_sazon(x, per_loja_0))
+lojas_df['periodo_1'] = lojas_df.encrypted_cnpj.apply(lambda x : count_sazon(x, per_loja_1))
+lojas_df['periodo_2'] = lojas_df.encrypted_cnpj.apply(lambda x : count_sazon(x, per_loja_2))
+lojas_df['periodo_3'] = lojas_df.encrypted_cnpj.apply(lambda x : count_sazon(x, per_loja_3))
+lojas_df['periodo_4'] = lojas_df.encrypted_cnpj.apply(lambda x : count_sazon(x, per_loja_4))
+
+lojas_df.to_csv("data/lojas_enc.csv", index=False)
--- prototipos/dbscan.py
+++ prototipos/dbscan.py
@@ -1,31 +1,42 @@
 import numpy as np
 import pandas as pd
-from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import scale
-from sklearn.decomposition import PCA
 from sklearn import metrics
-from sklearn.metrics import pairwise_distances
+from sklearn.decomposition import PCA
 
 lojas_df = pd.read_csv("data/lojas_enc.csv")
 lojas_df.encrypted_5_zipcode = pd.to_numeric(lojas_df.encrypted_5_zipcode, errors='coerce')
-
 subset = lojas_df[['encrypted_5_zipcode','produtos_vendidos','transacoes_total','faturamento_total']].dropna()
+
+#minmax
 subset = (subset - subset.min()) / (subset.max() - subset.min())
+#zscore
+#subset = (subset - subset.mean()) / subset.std()
 
 X = np.column_stack([subset.encrypted_5_zipcode, subset.produtos_vendidos,subset.transacoes_total,subset.faturamento_total])
-kmeans = KMeans(init='k-means++', n_clusters=3)
-kmeans.fit(X)
-predicted = kmeans.predict(X)
 
-plt.scatter(X[:,0], X[:,1], c=predicted)
-metrics.silhouette_score(X, predicted,metric='euclidean')
+clustering = DBSCAN(metric='euclidean', eps=0.3)
+clustering.fit(X)
+metrics.silhouette_score(X, clustering.labels_,metric='euclidean')
+
+pca = PCA(n_components=2)
+reduced = pca.fit_transform(X)
+plt.scatter(reduced[:,0], reduced[:,1], c=clustering.labels_)
 
 sazon_set = lojas_df[['encrypted_5_zipcode','periodo_0','periodo_1','periodo_2','periodo_3','periodo_4']].dropna()
 sazon_set = (sazon_set - sazon_set.min()) / (sazon_set.max() - sazon_set.min())
 
 X = np.column_stack([sazon_set.encrypted_5_zipcode, sazon_set.periodo_0,sazon_set.periodo_1,sazon_set.periodo_2,sazon_set.periodo_3,sazon_set.periodo_4])
-kmeans = KMeans(init='k-means++', n_clusters=5)
-kmeans.fit(X)
-predicted = kmeans.predict(X)
-metrics.silhouette_score(X, predicted,metric='euclidean')
+clustering = DBSCAN(metric='euclidean')
+clustering.fit(X)
+
+plt.scatter(X[:,0], X[:,1], c=clustering.labels_)
+metrics.silhouette_score(X, clustering.labels_,metric='euclidean')
+
+clientes = pd.read_csv("data/clientes_pf.csv")
+X = np.column_stack([clientes.total_produtos, clientes.valor_total])
+clustering = DBSCAN(metric='euclidean')
+clustering.fit(X)
+plt.scatter(X[:,0], X[:,1], c=clustering.labels_)
+metrics.silhouette_score(X, clustering.labels_,metric='euclidean')
